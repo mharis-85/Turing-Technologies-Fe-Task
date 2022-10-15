0cd4573ecb9276b6c5badd1c044abfa027ffce88
Ashfahan <work@ashfahan.com>
2022-10-15 21:36:29 +0500

    feat: add Login Page

--- src/pages/Login/Login.interfaces.ts
+++ src/pages/Login/Login.interfaces.ts
@@ -1,3 +1,8 @@
 export interface LoginProps {
   className?: string
 }
+
+export interface FormState {
+  email: string
+  password: string
+}
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,83 @@
-import { FC } from "react"
+import { AccountCircle, Lock, Visibility, VisibilityOff } from "@mui/icons-material"
+import { Button, IconButton, TextField } from "@mui/material"
+import { FC, useState } from "react"
+import { useForm } from "react-hook-form"
 
-import { LoginProps } from "./Login.interfaces"
+import { FormField } from "../../components/FormField"
+import { FormState, LoginProps } from "./Login.interfaces"
 
 export const Login: FC<LoginProps> = (props) => {
-  const { className } = props
+  const [showpassword, setShowpassword] = useState(false)
 
-  return <div className={className}>Login Works!</div>
+
+  const { control, handleSubmit } = useForm<FormState>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit = async (value: FormState) => {
+  }
+
+  return (
+    <section className="bg-white relative max-w-lg w-full container p-5 space-y-3 mx-auto">
+      <h3 className="text-2xl">Login</h3>
+      <div>Please enter your details</div>
+      <form className="space-y-5 pt-3" onSubmit={handleSubmit(onSubmit)}>
+        <FormField
+          control={control}
+          name="email"
+          label="User Name"
+          rules={{
+            required: { message: "Value is Required", value: true },
+            pattern: {
+              value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+              message: "Not a valid Email",
+            },
+          }}
+        >
+          <TextField
+            size="small"
+            type="email"
+            autoComplete="email"
+            fullWidth
+            placeholder="Email"
+            InputProps={{
+              startAdornment: <AccountCircle />,
+            }}
+          />
+        </FormField>
+
+        <FormField
+          control={control}
+          name="password"
+          label="Password"
+          rules={{
+            required: { message: "Value is Required", value: true },
+            minLength: { message: "Password Length should be 8", value: 8 },
+          }}
+        >
+          <TextField
+            size="small"
+            autoComplete="current-password"
+            fullWidth
+            placeholder="babayaga"
+            type={showpassword ? "text" : "password"}
+            InputProps={{
+              startAdornment: <Lock />,
+              endAdornment: (
+                <IconButton onClick={() => setShowpassword((prev) => !prev)}>
+                  {showpassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              ),
+            }}
+          />
+        </FormField>
+        <Button type="submit" variant="contained" fullWidth>
+          Log in
+        </Button>
+      </form>
+    </section>
+  )
 }
