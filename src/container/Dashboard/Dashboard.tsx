a75a91ed42bb8f2966f008ecc3a926d1f14a68a5
Ashfahan <work@ashfahan.com>
2022-10-15 23:39:32 +0500

    feat: add filter select

--- src/container/Dashboard/Dashboard.tsx
+++ src/container/Dashboard/Dashboard.tsx
@@ -1,9 +1,33 @@
-import { FC } from "react"
+import { FC, useState } from "react"
+import { Option, Select } from "../../components/Select"
 
 import { DashboardProps } from "./Dashboard.interface"
 
 export const Dashboard: FC<DashboardProps> = (props) => {
   const { className } = props
 
-  return <div className={className}>Dashboard Works!</div>
+  const [filter, setFilter] = useState("")
+
+  return (
+    <main className={`${className} bg-white flex-grow p-5 space-y-3`}>
+      <section>
+        <h3 className="text-2xl mb-10">Turing Technologies Frontend Test</h3>
+        <div className="flex items-center space-x-2">
+          <div>Filter by: </div>
+          <Select
+            className="min-w-[100px]"
+            value={filter}
+            onChange={(_, value) => value && setFilter(value as string)}
+          >
+            <Option value="" hidden>
+              Status
+            </Option>
+            <Option value="10">All</Option>
+            <Option value="20">Archive</Option>
+            <Option value="30">UnArchive</Option>
+          </Select>
+        </div>
+      </section>
+    </main>
+  )
 }
