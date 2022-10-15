4e4fd28761646e046e41879c553c641322d95c71
Ashfahan <work@ashfahan.com>
2022-10-15 21:36:07 +0500

    style: header

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react"
+import { Link } from "react-router-dom"
+import { URL_DASHBOARD } from "../../router/routes"
 
 import { HeaderProps } from "./Header.interfaces"
 
 export const Header: FC<HeaderProps> = () => {
   return (
-    <header className="border-b-[1px] bg-white flex-grid justify-between items-center py-2 px-4">
-      Header Works!
+    <header className="border-b-2 bg-white flex flex-wrap justify-between items-center py-4 px-4">
+      <Link to={URL_DASHBOARD}>
+        <img src="/Logo.png" alt="logo" className="h-auto w-full max-h-6" />
+      </Link>
     </header>
   )
 }
